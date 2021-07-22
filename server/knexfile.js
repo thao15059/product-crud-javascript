a7a939d034f557b4ffae5e41c9097d0a5d38cd72
thao15059 <thao15059@gmail.com>
2021-07-22 21:56:12 +0700

    Prepare for Deploy

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,4 +3,8 @@ module.exports = {
     client: "pg",
     connection: { user: "postgres", database: "ts_store", password: "1234" },
   },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+  },
 };
